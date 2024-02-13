61a03c06ccfec61f81efb4cbe0790027c33b1c1c
KWcouple <106459021+KWcouple@users.noreply.github.com>
2024-02-13 23:48:14 +0900

    Add files via upload

---
+++ reseource/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    // 갤러리 펼침 코드
+    const subs = document.getElementById("morebtn")
+    subs.addEventListener("click", function() {
+        if(subs.innerText === '더보기') {
+            subs.innerText = '닫기';
+        } else subs.innerText ='더보기';
+    });
+
+    Fancybox.bind("[data-fancybox]", { 
+        // Your options go here 
+    });
+
+    // 갤러리 펼침 코드
+    var bDisplay = true; function doDisplay(){ 	
+        var con = document.getElementById("img_more"); 	
+        if(con.style.display=='none'){ 		
+            con.style.display = 'block';
+        }else{
+            con.style.display = 'none';
+        }
+    }
+    $(function() {
+        $('#button1').click( function() {
+            if( $(this).html() == '더보기' ) {
+            $(this).html('닫기');
+            }
+            else {
+            $(this).html('더보기');
+            }
+        });
+    });
+});
